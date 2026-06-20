dc4de9388b97299bcf79ba336bdd862944711f4f
Juli Tiwari <julitiwari901@gmail.com>
2026-06-20 15:55:45 +0530

    Time: 9 ms (80.18%), Space: 19.4 MB (75.37%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        freq={}
+        for ch in s:
+            freq[ch]=freq.get(ch,0)+1
+        for ch in t:
+            freq[ch]=freq.get(ch,0) -1 
+        for value in freq.values():
+           if value != 0:
+             return False 
+        return True
+
